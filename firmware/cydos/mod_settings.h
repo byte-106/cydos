3d8341c45cc3669ded5ccfa172a13d7cdae9b12d
byte-106 <ym3006928@gmail.com>
2026-08-23 23:52:23 +0300

    Always show wifi_ssid/wifi_pass lines in settings.json as editable slots

--- firmware/cydos/mod_settings.h
+++ firmware/cydos/mod_settings.h
@@ -47,9 +47,9 @@ static void settingsBuildDoc(String &d) {
   sjSetValue(d, "brightness", String(kvGetInt("bl", 200)));
   sjSetValue(d, "invert", kvGetBool("inv", false) ? "true" : "false");
   String ssid = kvGet("wifi_ssid", "");
-  if (ssid.length()) sjSetValue(d, "wifi_ssid", "\"" + ssid + "\"");
+  sjSetValue(d, "wifi_ssid", "\"" + ssid + "\"");
   String pass = kvGet("wifi_pass", "");
-  if (pass.length()) sjSetValue(d, "wifi_pass", "\"" + pass + "\"");
+  sjSetValue(d, "wifi_pass", "\"" + pass + "\"");
   d = "{\n" + d + "\n}";
 }
 
